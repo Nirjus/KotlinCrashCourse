d1c090aa49e34439c5faa1dde092263bff194d45
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2025-07-30 22:29:02 +0530

    Add data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,3 +3,9 @@ package org.example
 fun main(){
     println("wassp dude!")
 }
+data class Person(name: String, age: Int)
+fun createPerson(){
+    val person1 = Person(name= "Ashu", age= 85)
+    println("name : ${person1.name}")
+    println(person1.age)
+}
